f0bdc0404ad18ba649183cb11fbadc583dd35862
Matt Pedersen <mpedersen2054@gmail.com>
2015-01-13 12:55:34 -0600

    add  service to count down from 5, if no user input it will auto search .username

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function() {
 var app = angular.module('githubViewer', []);
 
-var MainCtrl = function($scope, $http) {
+var MainCtrl = function($scope, $http, $interval) {
 
     var onUserSuccess = function(response) {
         $scope.user = response.data;
@@ -13,10 +13,21 @@ var MainCtrl = function($scope, $http) {
     var onRepoSuccess = function(response) {
         $scope.repos = response.data;
         $scope.username = '';
-    }
+    };
 
     var onError = function(reason) {
         $scope.error = "There was an error";
+    };
+
+    var decrementCountdown = function() {
+        $scope.countdown -= 1;
+        if ($scope.countdown < 1) {
+            $scope.search($scope.username);
+        }
+    };
+
+    var startCountDown = function() {
+        $interval(decrementCountdown, 1000, $scope.countdown);
     };
 
     $scope.search = function(username) {
@@ -24,8 +35,10 @@ var MainCtrl = function($scope, $http) {
             .then(onUserSuccess, onError)
     };
 
+    $scope.countdown = 5;
     $scope.username = "mpedersen2054";
     $scope.message = "hello from angular!";
+    startCountDown();
 };
 
 app.controller('MainCtrl', MainCtrl);
