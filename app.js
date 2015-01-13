54f330e4e96cfd29ca5bd84d272268cf1cf74815
Matt Pedersen <mpedersen2054@gmail.com>
2015-01-12 19:11:31 -0600

    add form and form ng-submit to search for the user from github api and return a user object

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 var app = angular.module('githubViewer', []);
 
 var MainCtrl = function($scope, $http) {
+
+    var onUser = function(response) {
+        $scope.user = response.data;
+    };
+
+    var onError = function(reason) {
+        $scope.error = "There was an error";
+    };
+
+    $scope.search = function(username) {
+        $http.get('https://api.github.com/users/' + username)
+            .then(onUser, onError)
+    };
+
     $scope.message = "hello from angular!";
 };
 
